3866ba37a852b7204e2ad6b9dd2ea5e83e20dfe6
david <michalcodavid@gmail.com>
2022-05-09 23:31:43 +0200

    init commit

---
+++ db_server.py
@@ -0,0 +1,63 @@
+import threading
+import time
+
+import grpc
+import concurrent
+from concurrent import futures
+import pymongo
+from pymongo import MongoClient
+import os
+import grpc
+import db_pb2
+import db_pb2_grpc
+USER = os.getenv('USER')
+PASSWORD = os.environ.get('PASSWORD')
+
+cluster = MongoClient("mongodb://"+USER+":"+PASSWORD+"@cluster0-shard-00-00.otcai.mongodb.net:27017,cluster0-shard-00-01.otcai.mongodb.net:27017,cluster0-shard-00-02.otcai.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-zkkws9-shard-0&authSource=admin&retryWrites=true&w=majority")
+mydb = cluster["test"]
+collection = mydb["test"]
+class DatabaseServiceServicer(db_pb2_grpc.DatabaseServiceServicer):
+
+    def GetBox(self,request,context):
+        return  collection.find({request},{})
+
+    def GetBoxes(self,request,context):
+
+        return  db_pb2.GetBoxesResponse(collection.find({},{}))
+
+    def CreateBox(self, request, context):
+        print("hi")
+        print(request,"hi2")
+        collection.insert_one(request)
+        return db_pb2.CreateBoxResponse
+
+    def UpdateBox(self, request, context):
+        print("we got UpdateBox")
+
+
+    def DeleteBox(self, request, context):
+        print("we got DeleteBox")
+
+
+    def GetBoxesInCategory(self, request, context):
+        print("we got GetBoxesInCategory")
+
+
+    def GetBoxesInTimeRange(self, request, context):
+        print("we got GetBoxesInTimeRange")
+
+
+def main():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    db_pb2_grpc.add_DatabaseServiceServicer_to_server(DatabaseServiceServicer(), server)
+    server.add_insecure_port("[::]:9999")
+    server.start()
+    try:
+        while True:
+            print("server on")
+            time.sleep(10)
+    except KeyboardInterrupt:
+        print("key board interrupt")
+        server.stop(0)
+if __name__ == "__main__":
+    main()
