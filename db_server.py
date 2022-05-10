1a3eef49976928b48b812863d92668c89bbde9c7
david <david@eachthing.com>
2022-05-10 08:49:14 +0200

    updated lib

--- db_server.py
+++ db_server.py
@@ -1,27 +1,37 @@
 import threading
 import time
 import json
+from datetime import datetime
 
 import grpc
 import concurrent
 from concurrent import futures
 import pymongo
+import pymongoarrow
 from pymongo import MongoClient
 import os
 import grpc
 import db_pb2
 import db_pb2_grpc
+import pymongoarrow as pma
+import pyarrow
+import pymongo
+import bson
+import pymongoarrow.monkey
 
-os.environ["USER"] = "david123"
-os.environ["PASSWORD"] = "david123"
+pymongoarrow.monkey.patch_all()
+
+os.environ['USER'] = 'david123'
+os.environ['PASSWORD'] = 'david123'
 
 USER = os.getenv('USER')
 PASSWORD = os.environ.get('PASSWORD')
 
 cluster = MongoClient(
     "mongodb://" + USER + ":" + PASSWORD + "@cluster0-shard-00-00.otcai.mongodb.net:27017,cluster0-shard-00-01.otcai.mongodb.net:27017,cluster0-shard-00-02.otcai.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-zkkws9-shard-0&authSource=admin&retryWrites=true&w=majority")
-mydb = cluster["test"]
-collection = mydb["test"]
+mydb = cluster.get_default_database()
+collection = mydb.get_collection('test')
+
 
 
 class DatabaseServiceServicer(db_pb2_grpc.DatabaseServiceServicer):
@@ -33,6 +43,8 @@ class DatabaseServiceServicer(db_pb2_grpc.DatabaseServiceServicer):
         return db_pb2.GetBoxesResponse(collection.find({}, {}))
 
     def CreateBox(self, request, context):
+        print(context)
+        print(self)
         collection.insert_one({"name": request.box.name, "id": request.box.id, "price": request.box.price,
                                "description": request.box.description, "category": request.box.category,
                                "quantity": request.box.quantity, "created_at": {"seconds":request.box.created_at.seconds,"nanos":request.box.created_at.nanos}})
